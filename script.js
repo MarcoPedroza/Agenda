dddc114cba8545113eac70578bebaceff5be4740
MarcoPedroza <mfpedrozap@correo.udistrital.edu.co>
2023-03-20 18:22:28 -0500

    crear elementos de forma dinámica

--- script.js
+++ script.js
@@ -3,7 +3,18 @@ const btn= document.querySelector("[data-form-btn]");
 const crearTarea = (e) => {
     e.preventDefault();
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
+    const value = input.value;
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement("li");
+    input.value = "";
+    const content = `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`
+    task.innerHTML = content;
+    list.appendChild(task);
+    task.classList.add("card");
 }
 
 btn.addEventListener("click", crearTarea);
